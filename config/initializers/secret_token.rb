9c08b3f16b9cd8db4acff04954b4c26d8831fefe
Charles Max Wood <chuck@teachmetocode.com>
2011-10-28 16:43:34 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rocketforge::Application.config.secret_token = '35906b5701a57c0d7808dfc4f55164f924e84f95519bde63e776e85ec1125db11d133a536788d8973adf43577c9d1c1401ea15730edf5a4a1916b9337932602c'
